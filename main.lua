48b14c84a6139e8b4d9173fd07af472bdbc75d0b
Tim Rodriguez <info@epistech.com>
2014-12-20 12:56:26 -0500

    Lasers!
    Feelings TK

--- main.lua
+++ main.lua
@@ -1,13 +1,33 @@
+local physics = require("physics")
+physics.start()
+
 -- print logs data to the console, has nothing to do with on-screen display.
 print( "Hello World!" )
 
 --Initialize the main text object
-local myTextObject = display.newText( "Hello World!", 160, 240, "Arial", 60)
+local myTextObject = display.newText( "+==/||\\==+", 160, 340, "Arial", 60)
 myTextObject:setFillColor(1,.65,0)
+
+local badguy = display.newText("@@", 35,75, "Courier", 40)
+physics.addBody(badguy, "kinematic", {density=0.01,friction=0.5, bounce=0.3})
+
+
+local function startGame()
+	print "game started..."
+	if (quitter == false and badguy.x < 300) then
+		badguy:translate(300)
+	elseif (quitter == false and badguy.x > 300) then
+		badguy:translate(-300)
+	elseif (quitter == true ) then
+		return true
+	end
+end
+startGame()
+
 
 -- Initialize score tracker
 local score = "0"
-local showScore = display.newText(score,300,50,"Courier",30)
+local showScore = display.newText(score,300,30,"Courier",30)
 
 --Event handler for touch event; mostly stolen
 function myTextObject:touch( event )
@@ -24,14 +44,36 @@ end
 
 --[[Totally un-factored functions that handle the gameplay; most of which are shoddily
 	constructed because they're an outgrowth of a tutorial project ]]--
+
+function fireLasers()
+	blaster = display.newImage("laserA.png", 6,9)
+	physics.addBody(blaster, "dynamic", {density=5.0,bounce=0.1,radius=10} )
+	blaster.gravityScale = 0
+	blaster.isBullet = true
+	blaster.x = myTextObject.x
+	blaster.y = myTextObject.y
+	transition.to(blaster, {time=1000, y = -50})
 	
+	blaster = nil
+end
+
+local function handleFireButton( event )
+	if (event.phase == "moved") then
+		fireLasers()
+		print("event: moved")
+	end
+	return true
+end
+
+myTextObject:addEventListener("touch", handleFireButton)
+
 function bgTap()
-  local x = 0 --math.random(-200,200)
+  local x = math.random(-200,200)
   local y = math.random(-200,200)
-  myTextObject:translate (x,y)
-  screenTap()
+  -- myTextObject:translate (x,y)
+  -- screenTap()
   scoreplus()
-  if myTextObject.x < -160 or myTextObject.y < -20 or myTextObject.x > 475 or myTextObject.y > 580 then
+  if myTextObject.x < -200 or myTextObject.y < -20 or myTextObject.x > 475 or myTextObject.y > 590 then
     endgame()
   end
 end
@@ -47,14 +89,27 @@ end
 function scoreplus()
   score = score + 1
   showScore.text = score
+  if (score > 100) then
+    endgame()
+  end
 end
+
+badguy:addEventListener( "collision", scoreplus )
+
 
 -- When the end-state is reached, display score.
 function endgame()
   -- was using print to test for "accurate" boundaries because of kinda weird screen tracking system.
   -- print(myTextObject.x,myTextObject.y)
+  quitter = true
   local finished = display.newText("Your final score is "..score, 150, 240, "Courier", 20)
+  finished:addEventListener("tap", removeScene)
+  
+  
 end
+
+  
+
 
 -- Add the "tap" event listener to myTextObject, and call "bgTap" when triggered.
 -- The difference between colons and periods to call functions versus data is a little tricky. Careful!
